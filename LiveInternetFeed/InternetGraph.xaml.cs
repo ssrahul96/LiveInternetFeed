f64befca827975c45fa0c84177dcf5025b93e37c
Rahul S S <rahul.ss@gofrugal.com>
2019-01-12 10:19:08 +0530

    added logfor net

--- LiveInternetFeed/InternetGraph.xaml.cs
+++ LiveInternetFeed/InternetGraph.xaml.cs
@@ -1,169 +1,159 @@
 ﻿using LiveCharts;
 using LiveCharts.Configurations;
-using LiveCharts.Wpf;
 using LiveInternetFeed.Model;
+using log4net;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
-using System.Linq;
 using System.Net;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
 
 namespace LiveInternetFeed
 {
-    public partial class LiveInternet : UserControl, INotifyPropertyChanged
-    {
-        private double _axisMax;
-        private double _axisMin;
-        private double _trend;
+	public partial class InternetGraph : UserControl, INotifyPropertyChanged
+	{
+		public static readonly ILog log = LogManager.GetLogger(typeof(InternetGraph));
+		private double _axisMax;
+		private double _axisMin;
+		private double _trend;
 
-        public LiveInternet()
-        {
-            InitializeComponent();
+		public InternetGraph()
+		{
+			InitializeComponent();
 
-            var mapper = Mappers.Xy<MeasureModel>()
-               .X(model => model.DateTime.Ticks)   //use DateTime.Ticks as X
-               .Y(model => model.Value);           //use the value property as Y
+			var mapper = Mappers.Xy<MeasureModel>()
+			   .X(model => model.DateTime.Ticks)   //use DateTime.Ticks as X
+			   .Y(model => model.Value);           //use the value property as Y
 
-            //lets save the mapper globally.
-            Charting.For<MeasureModel>(mapper);
+			//lets save the mapper globally.
+			Charting.For<MeasureModel>(mapper);
 
-            //the values property will store our values array
-            ChartValues = new ChartValues<MeasureModel>();
+			//the values property will store our values array
+			ChartValues = new ChartValues<MeasureModel>();
 
-            //lets set how to display the X Labels
-            DateTimeFormatter = value => new DateTime((long)value).ToString("hh:mm:ss");
+			//lets set how to display the X Labels
+			DateTimeFormatter = value => new DateTime((long)value).ToString("hh:mm:ss");
 
-            //AxisStep forces the distance between each separator in the X axis
-            AxisStep = TimeSpan.FromSeconds(10).Ticks;
-            //AxisUnit forces lets the axis know that we are plotting seconds
-            //this is not always necessary, but it can prevent wrong labeling
-            AxisUnit = TimeSpan.TicksPerSecond;
+			//AxisStep forces the distance between each separator in the X axis
+			AxisStep = TimeSpan.FromSeconds(10).Ticks;
+			//AxisUnit forces lets the axis know that we are plotting seconds
+			//this is not always necessary, but it can prevent wrong labeling
+			AxisUnit = TimeSpan.TicksPerSecond;
 
-            SetAxisLimits(DateTime.Now);
+			SetAxisLimits(DateTime.Now);
 
-            //The next code simulates data changes every 300 ms
+			//The next code simulates data changes every 300 ms
 
-            IsReading = false;
+			IsReading = false;
 
-            DataContext = this;
-        }
+			DataContext = this;
+		}
 
-        public ChartValues<MeasureModel> ChartValues { get; set; }
-        public Func<double, string> DateTimeFormatter { get; set; }
-        public double AxisStep { get; set; }
-        public double AxisUnit { get; set; }
+		public ChartValues<MeasureModel> ChartValues { get; set; }
+		public Func<double, string> DateTimeFormatter { get; set; }
+		public double AxisStep { get; set; }
+		public double AxisUnit { get; set; }
 
-        public double AxisMax
-        {
-            get { return _axisMax; }
-            set
-            {
-                _axisMax = value;
-                OnPropertyChanged("AxisMax");
-            }
-        }
-        public double AxisMin
-        {
-            get { return _axisMin; }
-            set
-            {
-                _axisMin = value;
-                OnPropertyChanged("AxisMin");
-            }
-        }
+		public double AxisMax
+		{
+			get { return _axisMax; }
+			set
+			{
+				_axisMax = value;
+				OnPropertyChanged("AxisMax");
+			}
+		}
 
-        public bool IsReading { get; set; }
+		public double AxisMin
+		{
+			get { return _axisMin; }
+			set
+			{
+				_axisMin = value;
+				OnPropertyChanged("AxisMin");
+			}
+		}
 
-        private void Read()
-        {
-            var r = new Random();
+		public bool IsReading { get; set; }
 
-            while (IsReading)
-            {
-                Thread.Sleep(MainModel.time);
-                var now = DateTime.Now;
+		private void Read()
+		{
+			var r = new Random();
 
-                try
-                {
-                    using (WebClient wc = new MyWebClient())
-                    {
-                        wc.DownloadString(MainModel.url);
+			while (IsReading)
+			{
+				Thread.Sleep(MainModel.time);
+				var now = DateTime.Now;
 
-                        _trend = (DateTime.Now - now).TotalMilliseconds;
-                    }
-                }
-                catch(Exception ex)
-                {
-                    _trend = 0;
-                    Process.Start(String.Format("\"{0}\"",MainModel.toastExe),String.Format("\"{0}\" \"{1}\"",MainModel.toastMessage,ex.Message));
-                }
+				try
+				{
+					using (WebClient wc = new MyWebClient())
+					{
+						wc.DownloadString(MainModel.url);
 
+						_trend = (DateTime.Now - now).TotalMilliseconds;
+					}
+				}
+				catch (Exception ex)
+				{
+					_trend = 0;
+					log.Error(ex.Message);
+					Process.Start(String.Format("\"{0}\"", MainModel.toastExe), String.Format("\"{0}\" \"{1}{2}{3}\"", MainModel.toastMessage, DateTime.Now, Environment.NewLine, ex.Message));
+				}
 
+				ChartValues.Add(new MeasureModel
+				{
+					DateTime = now,
+					Value = _trend
+				});
 
-                ChartValues.Add(new MeasureModel
-                {
-                    DateTime = now,
-                    Value = _trend
-                });
+				SetAxisLimits(now);
 
-                SetAxisLimits(now);
+				//lets only use the last 150 values
+				if (ChartValues.Count > 100) ChartValues.RemoveAt(0);
+			}
+		}
 
-                //lets only use the last 150 values
-                if (ChartValues.Count > 100) ChartValues.RemoveAt(0);
-            }
-        }
+		private void SetAxisLimits(DateTime now)
+		{
+			AxisMax = now.Ticks + TimeSpan.FromSeconds(1).Ticks; // lets force the axis to be 1 second ahead
+			AxisMin = now.Ticks - TimeSpan.FromSeconds(100).Ticks; // and 8 seconds behind
+		}
 
-        private void SetAxisLimits(DateTime now)
-        {
-            AxisMax = now.Ticks + TimeSpan.FromSeconds(1).Ticks; // lets force the axis to be 1 second ahead
-            AxisMin = now.Ticks - TimeSpan.FromSeconds(100).Ticks; // and 8 seconds behind
-        }
+		private void InjectStopOnClick(object sender, RoutedEventArgs e)
+		{
+			IsReading = !IsReading;
+			if (IsReading) Task.Factory.StartNew(Read);
+		}
 
-        private void InjectStopOnClick(object sender, RoutedEventArgs e)
-        {
-            IsReading = !IsReading;
-            if (IsReading) Task.Factory.StartNew(Read);
-        }
+		#region INotifyPropertyChanged implementation
 
-        #region INotifyPropertyChanged implementation
+		public event PropertyChangedEventHandler PropertyChanged;
 
-        public event PropertyChangedEventHandler PropertyChanged;
+		protected virtual void OnPropertyChanged(string propertyName = null)
+		{
+			if (PropertyChanged != null)
+				PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
 
-        protected virtual void OnPropertyChanged(string propertyName = null)
-        {
-            if (PropertyChanged != null)
-                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+		#endregion INotifyPropertyChanged implementation
 
-        #endregion
+		private class MyWebClient : WebClient
+		{
+			protected override WebRequest GetWebRequest(Uri uri)
+			{
+				WebRequest w = base.GetWebRequest(uri);
+				w.Timeout = MainModel.time * 1000;
+				return w;
+			}
+		}
 
-        private class MyWebClient : WebClient
-        {
-            protected override WebRequest GetWebRequest(Uri uri)
-            {
-                WebRequest w = base.GetWebRequest(uri);
-                w.Timeout = MainModel.time * 1000;
-                return w;
-            }
-        }
-
-        private void Exit_Click(object sender, RoutedEventArgs e)
-        {
-            Environment.Exit(0);
-        }
-    }
+		private void Exit_Click(object sender, RoutedEventArgs e)
+		{
+			Environment.Exit(0);
+		}
+	}
 }
--- LiveInternetFeed/LiveInternet.xaml.cs
+++ LiveInternetFeed/LiveInternet.xaml.cs
@@ -1,72 +1,62 @@
-﻿using InteractiveDataDisplay.WPF;
+﻿using LiveInternetFeed.Model;
+using log4net;
+using log4net.Config;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using LiveCharts;
-using LiveCharts.Wpf;
-using LiveCharts.Defaults;
-using LiveInternetFeed.Model;
-using System.Configuration;
+using System.IO;
 using System.Text.RegularExpressions;
+using System.Windows;
 
 namespace LiveInternetFeed
 {
-    public partial class MainWindow : Window
-    {
-        private const String appName = "LiveInternet";
-        public MainWindow()
-        {
-            InitializeComponent();
-            Uri uriResult;
-            if (ValidHttpURL(Convert.ToString(System.Configuration.ConfigurationManager.AppSettings["samurl"]), out uriResult))
-                MainModel.url = uriResult?.AbsoluteUri;
-            else
-            {
-                MessageBox.Show("URL is not Proper", appName);
-                Environment.Exit(0);
-                return;
+	public partial class LiveInternet : Window
+	{
+		private const String appName = "LiveInternet";
+		public static readonly ILog log = LogManager.GetLogger(typeof(LiveInternet));
 
-            }
-            try
-            {
-                MainModel.time = 1000 * Int32.Parse(System.Configuration.ConfigurationManager.AppSettings["time"]);
-            }
-            catch
-            {
-                MessageBox.Show("Time is not Proper", appName);
-                Environment.Exit(0);
-                return;
-            }
+		public LiveInternet()
+		{
+			InitializeComponent();
+			XmlConfigurator.Configure(new FileInfo(System.IO.Path.Combine(Environment.CurrentDirectory, "LiveInternetFeed.exe.config")));
+			Uri uriResult;
+			if (ValidHttpURL(Convert.ToString(System.Configuration.ConfigurationManager.AppSettings["samurl"]), out uriResult))
+				MainModel.url = uriResult?.AbsoluteUri;
+			else
+			{
+				MessageBox.Show("URL is not Proper", appName);
+				Environment.Exit(0);
+				return;
+			}
+			try
+			{
+				MainModel.time = 1000 * Int32.Parse(System.Configuration.ConfigurationManager.AppSettings["time"]);
+			}
+			catch
+			{
+				MessageBox.Show("Time is not Proper", appName);
+				Environment.Exit(0);
+				return;
+			}
 
-        }
+			log.Info(String.Format("URL : {0}", MainModel.url));
+			log.Info(String.Format("TIMEOUT : {0}", MainModel.time));
+		}
 
-        public bool ValidHttpURL(string s, out Uri resultURI)
-        {
-            if (!Regex.IsMatch(s, @"^https?:\/\/", RegexOptions.IgnoreCase))
-                s = "http://" + s;
+		public bool ValidHttpURL(string s, out Uri resultURI)
+		{
+			if (!Regex.IsMatch(s, @"^https?:\/\/", RegexOptions.IgnoreCase))
+				s = "http://" + s;
 
-            if (Uri.TryCreate(s, UriKind.Absolute, out resultURI))
-                return (resultURI.Scheme == Uri.UriSchemeHttp ||
-                        resultURI.Scheme == Uri.UriSchemeHttps);
+			if (Uri.TryCreate(s, UriKind.Absolute, out resultURI))
+				return (resultURI.Scheme == Uri.UriSchemeHttp ||
+						resultURI.Scheme == Uri.UriSchemeHttps);
 
-            return false;
-        }
-    }
+			return false;
+		}
+	}
 
-    public class MeasureModel
-    {
-        public DateTime DateTime { get; set; }
-        public double Value { get; set; }
-    }
+	public class MeasureModel
+	{
+		public DateTime DateTime { get; set; }
+		public double Value { get; set; }
+	}
 }
